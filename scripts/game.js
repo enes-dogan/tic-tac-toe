3f278011c55461844fa738a29e1378dc9eacdccf
Enes-Dogan <enesdogan210@gmail.com>
2023-01-07 23:29:46 +0300

    active player logic

--- scripts/game.js
+++ scripts/game.js
@@ -7,3 +7,17 @@ function startNewGame() {
     gameAreaElement.style.display = 'block';
   }
 }
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+}
+
+function selectGameField(event) {
+  event.target.textContent = players[activePlayer].symbol; //starts with players[0]
+  event.target.classList.add('disabled');
+  switchPlayer();
+}
