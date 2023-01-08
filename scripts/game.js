3d18754780281d3c718f0c97f9b55a3658afbbd3
Enes-Dogan <enesdogan210@gmail.com>
2023-01-08 23:46:05 +0300

    checking for a winner logic

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,7 @@ function startNewGame() {
     return;
     // return don't needed because alert() method already blocks further code execution
   } else {
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = 'block';
   }
 }
@@ -14,10 +15,70 @@ function switchPlayer() {
   } else {
     activePlayer = 0;
   }
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-  event.target.textContent = players[activePlayer].symbol; //starts with players[0]
-  event.target.classList.add('disabled');
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (selectedField.tagName !== 'LI') {
+    return;
+  }
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Please select an empty field.');
+    return;
+  } else {
+    selectedField.textContent = players[activePlayer].symbol; //starts with players[0]
+    selectedField.classList.add('disabled');
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+    console.log(gameData);
+  }
+  const winnerId = checkForGameOver();
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][0] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // Checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Checking the \ for equality
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+  // Checking the / for equality
+  if (
+    gameData[0][2] > 0 &&
+    gameData[0][2] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][0]
+  ) {
+    return gameData[0][2];
+  }
+  return 0;
 }
